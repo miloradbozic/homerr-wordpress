007daae4b7817c4d1605f20492c83c7911aa63d6
Milorad Bozic <miloradbozic29@gmail.com>
2018-04-19 16:23:40 +0200

    part2

--- wp-content/themes/homerr/template-parts/page-home/content-part2.php
+++ wp-content/themes/homerr/template-parts/page-home/content-part2.php
@@ -1,3 +1,43 @@
-<div>
-    pt2
+<div class="section">
+    <h1 class="title"><span class="emphasized">Wat</span> is <span class="emphasized emphasized-alternate">Hommer</span></h1>
+    <div class="content">
+    Ons sociale netwerk bestaat uit Homerrs en HomerrPoints.
+    Homerrs zijn particulieren zoals bijvoorbeeld buurmannen of -vrouwen die regelmatig thuis zijn en voor jou een pakketje kunnen aannemen.
+    HomerrPoints zijn lokale ondernemingen zoals bijvoorbeeld een koffietentje of bakker
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="image-container">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/home/part2-image1.png"/>
+                    <div class="overlay"></div>
+                    <div class="image-title">Een of andere wijsheid van Juriaan Matthijssen</div>
+                </div>
+                <div class="image-description">Ons sociale netwerk bestaat uit Homerrs en HomerrPoints. Homerrs zijn particulieren zoals bijvoorbeeld buurmannen </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="image-container">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/home/part2-image2.png"/>
+                    <div class="overlay"></div>
+                    <div class="image-title">Een of andere wijsheid van Juriaan Matthijssen</div>
+                </div>
+                <div class="image-description">Ons sociale netwerk bestaat uit Homerrs en HomerrPoints. Homerrs zijn particulieren zoals bijvoorbeeld buurmannen </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="image-container">
+                <div class="image">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/home/part2-image3.png"/>
+                    <div class="overlay"></div>
+                    <div class="image-title">Een of andere wijsheid van Juriaan Matthijssen</div>
+                </div>
+                <div class="image-description">Ons sociale netwerk bestaat uit Homerrs en HomerrPoints. Homerrs zijn particulieren zoals bijvoorbeeld buurmannen </div>
+            </div>
+        </div>
+    </div>
+    <div class="row" style="margin-top: 60px;">
+        <a class="btn-default">Vraag informatie aan</a>
+    </div>
 </div>
