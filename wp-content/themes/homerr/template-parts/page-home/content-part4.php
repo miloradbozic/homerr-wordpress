c6a2a753085cfdf9359803be8ef7d58e5c22824b
Milorad Bozic <miloradbozic29@gmail.com>
2018-04-20 11:20:48 +0200

    part4

--- wp-content/themes/homerr/template-parts/page-home/content-part4.php
+++ wp-content/themes/homerr/template-parts/page-home/content-part4.php
@@ -1,3 +1,34 @@
-<div>
-    pt4
+<div class="section slider-section">
+    <h1 class="title">Het <span class="emphasized">netwerk</span> van
+    <span class="emphasized emphasized-alternate">Homerr</span></h1>
+
+    <div class="map-content">
+        <div class="row row-distanced">
+            <div class="col-md-6">
+                <div class="sub-title emphasized">Gemak</div>
+                <div class="content">Nooit meer thuis hoeven zijn voor een bezorging. Stuur je pakketje rechtstreeks naar een Homerr
+                </div>
+                <div class="link">Het gemak van Homerr</div>
+            </div>
+            <div class="col-md-6">
+                <div class="sub-title emphasized">Betrouwbaar</div>
+                <div class="content">Een sociaal netwerk van buurtgenoten en winkeliers
+                </div>
+                <div class="link">Het gemak van Homerr</div>
+            </div>
+        </div>
+        <div class="row row-distanced">
+        <div class="col-md-6">
+                <div class="sub-title emphasized">Duurzaam</div>
+                <div class="content">Minder CO2 uitstoot, doordat logistieke dienstverleners nog maar 1 keer hoeven te rijden
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="sub-title emphasized">Tracking</div>
+                <div class="content">Controle op je bestelling, door automatische berichtgeving van je Homerr
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="overlay"></div>
 </div>
