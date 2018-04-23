ab3d58f547f9816f16402aea7a0dd76613eb14e9
Milorad Bozic <miloradbozic29@gmail.com>
2018-04-23 16:37:55 +0200

    bg hommer

--- wp-content/themes/homerr/template-parts/page-home/content-part4.php
+++ wp-content/themes/homerr/template-parts/page-home/content-part4.php
@@ -9,15 +9,13 @@
                     <div class="flex-image">
                     <img src="<?php echo get_template_directory_uri(); ?>/images/home/home-slider-1.png" alt="" />
                     </div>
-                    <div class="image-text">
+                    <div class="image-title">
                         <span>Tonnie & co</span>
                         <div class="description">"Door <span class="emphasized emphasized-alternate">Homerr</span> ben ik nu miljonair"</div>
                     </div>
-                    
             </div>
 
             <div class="image-item">
-                
                     <div class="flex-image">
                     <img src="<?php echo get_template_directory_uri(); ?>/images/home/home-slider-2.png" alt="" />
                     </div>
@@ -26,7 +24,6 @@
                         <div class="description">"Gisteren geverifieerd <span class="emphasized emphasized-alternate">Homerr</span> geworden, dit maakt mij hele grote blij"
                     </div>
                 </div>
-                   
             </div>
             <div class="image-item">
                     <div class="flex-image">
@@ -44,47 +41,6 @@
                 
             </div>
         </div>
-        
-        <!--
-        <div class="row">
-            <div class="col-md-3">
-                <div class="image-container2">
-                    <div class="image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/home/home-slider-1.png"/>
-                    </div>
-                    <div class="image-title">Tonnie & co</div>
-                    <div class="image-description">"Door Homerr ben ik nu miljonair"</div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="image-container2">
-                    <div class="image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/home/home-slider-2.png"/>
-                    </div>
-                    <div class="image-title">Langendijk</div>
-                    <div class="image-description">"Gisteren geverifieerd Homerr geworden, dit maakt mij hele grote blij"</div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="image-container2">
-                    <div class="image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/home/home-slider-3.png"/>
-                    </div>
-                    <div class="image-title">Delicious store</div>
-                    <div class="image-description">"M'n hand past niet meer in m'n broekzak omdat die vol zit met euro's dankzij Homerr"</div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="image-container2">
-                    <div class="image">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/home/home-slider-4.png"/>
-                    </div>
-                    <div class="image-title">Elegance</div>
-                    <div class="image-description">"Gelijk even een wasje draaien terwijl je je pakketje op komt halen, door Homerr heb ik veel meer klanten"</div>
-                </div>
-            </div>
-        </div>
-        -->
     </div>
     <div class="slider-wrapper">
         <div class="slider">
@@ -94,7 +50,11 @@
             <div class="slider-link"></div>
             <div class="slider-btn-right"></div>
         </div>
-        <div class="slider-text"></div>
+        <div class="slider-text">
+            Ons sociale netwerk bestaat uit Homerrs en HomerrPoints. Homerrs zijn particulieren zoals bijvoorbeeld buurmannen of -vrouwen die regelmatig thuis zijn en voor jou een pakketje kunnen aannemen. HomerrPoints zijn lokale ondernemingen zoals bijvoorbeeld een koffietentje of bakker die tot minimaal 20 uur ‘s avonds geopend zijn. Daardoor is er altijd een punt dicht bij jou in de buurt om je pakketje op te halen.
+        </div>
     </div>
-
+    <div class="row wrapVraagInformationBtn" style="margin-top: 60px;">
+        <a class="btn-default">Het netwerk van Homerr</a>
+    </div>
 </div>
