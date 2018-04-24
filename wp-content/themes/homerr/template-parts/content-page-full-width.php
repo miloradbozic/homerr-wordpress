7474bd0e7911c855dc2d27609fbb04608db557ff
vladimir021 <vladimir.grujic021@gmail.com>
2018-04-24 11:29:15 +0200

    Adding new background-style in navigation bar

--- wp-content/themes/homerr/template-parts/content-page-full-width.php
+++ wp-content/themes/homerr/template-parts/content-page-full-width.php
@@ -12,10 +12,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php if (has_post_thumbnail( $post->ID ) ): ?>
 			<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-				<header class="entry-header entry-header-background" style="background-image: url('<?php echo $image[0]; ?>')">
-					<div class="container height-js">
-						<div class="center-elements">
-							<div class="header-content-area">
+				<header class="entry-header entry-header-background home-slider" style="background-image: url('<?php echo $image[0]; ?>')">
+					<div class="container">
+						<!-- <div class="center-elements"> -->
+							<div class="header-content">
 								<div class="header-content-area-inner">
 									<?php if( is_front_page() ) : ?>
 									<?php if (get_field('page_title_override')) {
@@ -50,7 +50,7 @@
 								  <?php endif; ?>
 								</div>
 							</div>
-						</div>
+						<!-- </div> -->
 					</div>
 			<?php else: ?>
 				<header class="entry-header">
