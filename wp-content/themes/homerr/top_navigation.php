b61c77020716991418eaa0470cafcdb16fb165a8
vladimir021 <vladimir.grujic021@gmail.com>
2018-05-07 17:41:23 +0200

    Create new mobile style for navigation bar on mobile device and bug fixing

--- wp-content/themes/homerr/top_navigation.php
+++ wp-content/themes/homerr/top_navigation.php
@@ -8,7 +8,7 @@
 <nav class="navbar navbar-default navbar-fixed-top transparent-header"> <!-- navbar-static-top -->
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" onclick="displayMobileNavBar()">
         <span class="sr-only"><?php _e( 'Toggle navigation', 'homerr' ); ?></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -22,21 +22,47 @@
        <?php foreach( $top_left_menu_items as $left_item ) : $left_active = $title == $left_item->title ? 'active' : '';  ?>
                 <li><a class="<?php echo $left_active; ?> " href="<?php echo $left_item->url ?>"><?php echo $left_item->title ?></a></li>
             <?php endforeach; ?>
-            <?php do_action('wpml_add_language_selector'); ?>
+            
       
             
             <?php foreach( $top_right_menu_items as $right_item ) : $right_active = $title == $right_item->title ? 'active' : ''; ?>
                 <li><a class="<?php echo $right_active; ?>" href="<?php echo $right_item->url ?>"><?php echo $right_item->title ?></a></li>
             <?php endforeach; ?>
+            <div>
+              <button id="closeBtn" type="button" class="navbar-toggle collapsed closeBtn"  data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" onclick="hideNavbar()">
+            </div>
         </ul>
       <?php endif; ?>
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only"><?php _e( 'Toggle navigation', 'homerr' ); ?></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+      
+        
       </button>
     </div><!--/.nav-collapse -->
   </div>
 </nav>
 <!-- End of Top navigation -->
+<script>
+  function displayMobileNavBar() {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+      console.log('mobile');
+      $(".navbar-header").css("display", "none");
+      $(".homeNetwerk li:first-child").css("display", "block");
+      $(".navbar-collapse").css({"max-height": "none", "background": "white", "opacity": "1"});
+      $(".navbar-collapse").addClass("mobileNavHeight");
+      $(".navbar-default .navbar-nav>li>a").css({"text-align": "center", "text-transform": "uppercase", "font-size": "30px", "line-height": "1em", "color": "black"});
+      $(".navbar-default .navbar-nav>li").css({"margin-bottom": "5%"});
+      $(".navbar-default .navbar-nav>li>a").removeClass('active');
+      $(".nav.navbar-nav.navbar-right > li:last-of-type a").css({"margin-left": "0px"});
+      $(".homeNetwerk li:last-child").css({"border": "0px solid"});
+      $(".navbar-default .navbar-toggle").css({"border-color": "transparent", "text-align": "center"});
+      $(".closeBtn").addClass("imageCloseBtn");
+      $("#closeBtn").css("display", "block");
+      $(".homeNetwerk").css({"display": "flex", "flex-direction": "column", "justify-content": "space-around", "height": "100%"});
+    } else {
+      console.log('desktop');
+    }
+  }
+
+  function hideNavbar() {
+    $(".navbar-header").css("display", "block");
+  }
+</script>
